b4d04b60cc8b3679bb5e474e5670e23684d861e8
Brijeshkumar Yadav <by9714322072@gmail.com>
2023-12-14 12:20:54 +0530

    feat: DB connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import 'dotenv/config';
+
+import connectDB from './db/index.js';
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`App start on url :: http://localhost:${PORT}`);
