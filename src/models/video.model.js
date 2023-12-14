0c4b02b52fdfc24c3adf3f87f588bbe666f88453
Brijeshkumar Yadav <by9714322072@gmail.com>
2023-12-14 14:37:20 +0530

    feat: create user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+import aggregatePaginate from 'mongoose-aggregate-paginate-v2';
+import { Schema, model } from 'mongoose';
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Number,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    deletedAt: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(aggregatePaginate);
+
+export default Video = model('Video', videoSchema);
